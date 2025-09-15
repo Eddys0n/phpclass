eca9927fb6a373709df31b62844e76d0727cd794
Edison Medina <mcyfabox@gmail.com>
2025-09-15 18:39:12 -0500

    class update public website

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,16 @@
+
+
 <ul>
-    <li><a href="/">Homepage</a></li>
-    <li><a href="/loops/">Loops</a></li>
-    <li><a href="#">CountDown</a></li>
-    <li><a href="#">Other</a></li>
+    <li>
+        <a href="/">Homepage</a>
+    </li>
+    <li>
+        <a href="/loops/">Loops</a>
+    </li>
+    <li>
+        <a href="#">CountDown</a>
+    </li>
+    <li>
+        <a href="#">Other</a>
+    </li>
 </ul>
--- loops/index.php
+++ loops/index.php
@@ -24,18 +24,38 @@
     echo $num1;
 
     $i = 1;
-
     while($i<7){
-        echo "<h$i>Hello World</h$i>";
+        echo "<h$i>Test</h$i>";
         $i++;
     }
-
     $i = 6;
-
     while($i>0){
-        echo "<h$i>Hello World</h$i>";
+        echo "<h$i>Test</h$i>";
         $i--;
     }
+    for ($i = 1; $i <= 6; $i++){
+        echo "<h$i>test</h$i>";
+    }
+    for ($i = 6; $i > 1; $i--){
+        echo "<h$i>test</h$i>";
+    }
+
+    $a = 100;
+    $b = 50;
+
+    //echo "<p>" . ($a+$b) . "</p>";
+    $firstName = "eDiSon";
+    $lastName = "meDiNa";
+
+    //$fullName = strtolower("$firstName $lastName");
+    $fullName = strtolower($firstName) .  " " . strtolower($lastName);
+
+    //echo $fullName;
+    echo strtolower($fullName);
+
+    //var_dump(str_split($fullName));
+
+    echo $fullName[0];
 
     ?>
 </main>
