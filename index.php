0f376359256135a20e0abc3d535491af95e2c4b4
Edison Medina <mcyfabox@gmail.com>
2025-09-15 12:19:23 -0500

    updated public website

--- index.php
+++ index.php
@@ -6,8 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edison's Website</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
 </head>
 <body>
-<h1>Hello World</h1>
+<header>
+
+    <h1>PHP Class - Edison Medina</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="/">Homepage</a></li>
+        <li><a href="/">Loops</a></li>
+        <li><a href="/">CountDown</a></li>
+        <li><a href="/">Other</a></li>
+    </ul>
+</nav>
+<main>
+    <img src="images/edison.jpg">
+    <br />
+    <br />
+    <p>Learning to code</p>
+    <p>Dedicated and bilingual IT professional pursuing a degree in Software Development at Fox Valley Technical College. Experienced in help desk support for educational and healthcare institutions. Skilled in troubleshooting, customer service, and technical problem-solving with a focus on creating positive user experiences.
+    </p>
+    <br />
+    <br />
+</main>
+<footer>
+    <p>&copy; 2020x EdisonMedina.com</p>
+</footer>
+
 </body>
 </html>
