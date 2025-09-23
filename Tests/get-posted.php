0f116b6119c24ec9104a199a96d71c4086491f9d
Edison Medina <mcyfabox@gmail.com>
2025-09-22 19:12:07 -0500

    finished 8 ball

---
+++ Tests/get-posted.php
@@ -0,0 +1,25 @@
+<?php
+    // if (isset($_POST['firstName'])){
+    //   $firstName = $_POST['firstName'];
+    //}
+    //else{
+    //  $firstName = "";
+    //}
+    $firstName = $_POST['firstName'] ?? '';
+    $lastName = $_POST['lastName'] ?? '';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Testing Get and Post</title>
+</head>
+<body>
+    <h2>Welcome <?=$firstName?><?=$lastName?></h2>
+    <form method="post">
+        <input type="text" name="first Name" id="firstName" value="<?=$firstName?>" />
+        <input type="text" name="last Name" id="lastName" value="<?=$lastName?>" />
+        <input type="submit" value="Submit Data"/>
+    </form>
+</body>
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,7 @@
 
-
+<?php
+$isMagic8Ball = $_SERVER['REQUEST_URI'] == '/magic-8ball' ? 'selected' : '';
+?>
 <ul>
     <li>
         <a href="/">Homepage</a>
@@ -8,9 +10,12 @@
         <a href="/loops/">Loops</a>
     </li>
     <li>
-        <a href="#">CountDown</a>
+        <a href="/countdown/">CountDown</a>
     </li>
     <li>
         <a href="#">Other</a>
+    </li>
+    <li class="<?=$isMagic8Ball?>">
+        <a href="/magic-8ball">Magic 8 Ball</a>
     </li>
 </ul>
